632008cd9fb549fd2fec25af9d439121ab64128c
smv5047 <smv5047@gmail.com>
2020-02-25 16:31:39 -0500

    created and updated nav and header

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Jumbotron, Button } from "reactstrap";
+
+function Header() {
+  const handleClick = () => {};
+
+  return (
+    <>
+      <Jumbotron>
+        <h1 className="display-2"> DJ Galavance </h1>
+
+        <Button color="primary" onClick={handleClick}>
+          Press to Party!
+        </Button>
+      </Jumbotron>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from "reactstrap";
+
+const MainNavbar = props => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" light expand="md">
+        <NavbarBrand href="/">DJ Galavance</NavbarBrand>
+        {/* <NavbarToggler onClick={toggle} /> */}
+        <NavLink href="/">About</NavLink>
+        <NavLink href="/">Book</NavLink>
+        <NavLink href="/">Music</NavLink>
+        <NavLink href="/">Testamonials</NavLink>
+      </Navbar>
+    </div>
+  );
+};
+
+export default MainNavbar;
