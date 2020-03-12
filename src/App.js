3ba8c2041b65cc0f89d24d392739ad88b2d8ee2b
smv5047 <smv5047@gmail.com>
2020-03-12 19:24:17 -0400

    form updates

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <Mainnavbar />
       <Header />
+      <ContactForm />
     </div>
   );
 }
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { withFormik, Form, Field, yupToFormErrors } from "formik";
+import { Button, FormGroup, Label, Input, FormText} from 'reactstrap'
 import * as yup from "yup";
 
 function ContactForm() {
@@ -11,7 +12,19 @@ function ContactForm() {
         <Field type="text" name="first_name" placeholder="John" />
         <Field type="text" name="last_name" placeholder="Doe" />
         <Field type="email" name="email" placeholder="John.Doe@gmail.com" />
-        <button type="submit">Submit</button>
+        <Field component="select" name="event_type">
+            <option value="" disabled>Select Event Type:</option>
+            <option value="wedding">Wedding</option>
+            <option value ="corporate_event">Corporate Event</option>
+            <option value="private_event">Private Event</option>
+            <option value="club_or_bar">Club or Bar</option>
+            <option value="other">Other</option>
+        </Field>
+        <FormText color="muted">
+          Please enter any event details below.
+        </FormText>
+        <Input type="textarea" name="text" id="exampleText" />
+        <Button type="submit">Submit</Button>
       </Form>
     </div>
   );
