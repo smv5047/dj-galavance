69c94c0363eb4b9ab15fafc70e9c672f27885b5c
smv5047 <smv5047@gmail.com>
2020-03-13 16:25:04 -0400

    contact form update

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { withFormik, Form, Field, yupToFormErrors } from "formik";
-import { Button, FormGroup, Label, Input, FormText} from 'reactstrap'
+import { Button, FormGroup, Label, Input, FormText } from "reactstrap";
 import * as yup from "yup";
 
 function ContactForm() {
@@ -13,16 +13,16 @@ function ContactForm() {
         <Field type="text" name="last_name" placeholder="Doe" />
         <Field type="email" name="email" placeholder="John.Doe@gmail.com" />
         <Field component="select" name="event_type">
-            <option value="" disabled>Select Event Type:</option>
-            <option value="wedding">Wedding</option>
-            <option value ="corporate_event">Corporate Event</option>
-            <option value="private_event">Private Event</option>
-            <option value="club_or_bar">Club or Bar</option>
-            <option value="other">Other</option>
+          <option value="" disabled>
+            Select Event Type:
+          </option>
+          <option value="wedding">Wedding</option>
+          <option value="corporate_event">Corporate Event</option>
+          <option value="private_event">Private Event</option>
+          <option value="club_or_bar">Club or Bar</option>
+          <option value="other">Other</option>
         </Field>
-        <FormText color="muted">
-          Please enter any event details below.
-        </FormText>
+        <FormText color="muted">Please enter any event details below.</FormText>
         <Input type="textarea" name="text" id="exampleText" />
         <Button type="submit">Submit</Button>
       </Form>
@@ -44,4 +44,5 @@ export default withFormik({
     last_name: yup.string()
     // email: yup.email()
   })
+  // onSubmit
 })(ContactForm);
